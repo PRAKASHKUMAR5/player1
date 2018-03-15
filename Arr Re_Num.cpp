d0a9d2ffcec28895bf1ad100ca2f576b795f8b98
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-15 22:25:36 +0530

    Add files via upload

---
+++ Arr Re_Num.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the limits :"<<endl;
+	cin>>n;
+	int arr[n],p[n];
+	for(int i=0;i<=n;i++)
+	{
+		cin>>arr[i];
+	}
+	sort(arr,arr+n);
+	for(int j=0;j<=n;j++)
+	{
+		if(arr[j]==arr[j+1])
+		{
+			cout<<arr[j];
+		}
+	}	
+   return 0;
+}
