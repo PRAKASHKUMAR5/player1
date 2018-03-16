ad2bfa7aa151b97a3d4258cc1b009028923b95f8
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-03-16 17:17:13 +0530

    Add files via upload

---
+++ s.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the limits and values:"<<endl;
+	cin>>n;
+	int arr[n],p[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	sort(arr,arr+n);
+	for(int j=0;j<n;j+=2)
+	{
+			if(arr[j]!=arr[j+1])
+		   {
+			 cout<<arr[j];
+			 break;
+		   }
+		
+	}	
+   return 0;
+}
