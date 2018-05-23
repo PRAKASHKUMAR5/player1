2f518cbcd6a5561f939ce28575e444ca64c2d3db
PRAKASHKUMAR5 <36396155+PRAKASHKUMAR5@users.noreply.github.com>
2018-05-23 15:02:41 +0530

    Create Check if it is sorted.cpp

---
+++ Check if it is sorted.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,count=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<=arr[i+1])
+        {
+            count++;
+        }
+    }
+    if(count==n-1)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+    return 0;
+}
